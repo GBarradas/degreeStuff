55fdcb71a46b19666bdbecf52b99e837338d5228
Gonçalo Barradas <78180811+GBarradas@users.noreply.github.com>
2021-10-29 19:01:50 +0100

    Update Tabelas.sql

--- BD/Trabalho1/Tabelas.sql
+++ BD/Trabalho1/Tabelas.sql
@@ -36,4 +36,24 @@ CREATE TABLE Continent(
   NAME VARCHAR(50) CONSTRAINT ContinentKey primary key,
  Area DECIMAL(10)
 );
-  
+ create table Encompasses(
+   	country varchar(5),
+   	continent varchar(50),
+   	Percentage DECIMAL CONSTRAINT ContinentPercent
+   	CHECK ((Percentage > 0) AND (Percentage <= 100)),
+ 	constraint EncodeKey primary key (country, continent)
+ );
+ create table Organization(
+   	name varchar(100),
+   abreviation varchar(30),
+   city varchar(50),
+   county varchar(5),
+   province varchar(100),
+   dateOfEstablisment date,
+   CONSTRAINT OrganizationKey PRIMARY key (name,country)
+);
+   
+   
+   
+                                                      
+
