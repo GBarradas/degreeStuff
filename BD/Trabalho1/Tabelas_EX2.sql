1be81c536e5e0fa0f444bc141666c0420eef1f5d
Gonçalo Barradas <78180811+GBarradas@users.noreply.github.com>
2021-10-30 18:45:52 +0100

    Update Tabelas_EX2.sql

--- BD/Trabalho1/Tabelas_EX2.sql
+++ BD/Trabalho1/Tabelas_EX2.sql
@@ -1,12 +1,12 @@
 --Tabelas ex2
 
-drop table membro,gostade;
 create table membro(
 	nome varchar(50) CONSTRAINT membroKey PRIMARY key,
   	email  varchar(50),
   	ano DECIMAL(4)
 );
 create table gostade(
-	nome varchar(50) PRIMARY key,
+	nome varchar(50),
+  FOREIGN  key(nome) REFERENCES  membro(nome),
   	country varchar(50)
 );
