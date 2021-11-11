19a5973754314f3e21d5cae516a5c36dec08a7b1
Gonçalo Barradas <78180811+GBarradas@users.noreply.github.com>
2021-11-11 16:07:33 +0000

    Add files via upload

---
+++ P2/AbstractProvider.java
@@ -0,0 +1,5 @@
+package Trabalho;
+
+public class AbstractProvider {
+    
+}
---
+++ P2/Cipher.java
@@ -0,0 +1,29 @@
+package Trabalho;
+import java.lang.Character;
+import java.text.Normalizer;
+import java.lang.String;
+
+public class Cipher {
+    public static String normalize(String naturalText){
+        String normalizado=new String();
+
+
+        return normalizado;
+    }
+    public static void main(String[] Args){
+        //System.out.println(isLetter("."));
+    }
+    
+    
+}
+/* java.lang.Character
+        boolean isLetter(char ch)
+        boolean isDigit(char ch)
+        boolean isUpperCase(char ch)
+        char toLowerCase(char ch)
+
+    java.text.Normalizer;
+        Normalizer.normalize(str,Normalizer.Form.NFD)
+    java.text.StringBuilder
+        deleteCharAt(int index);
+*/
