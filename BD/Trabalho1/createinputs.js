aab51ae86a1dc80378c339f60d8dd1f7ccb63d5f
Gbarradas <gbarradas1@hotmail.com>
2021-12-22 20:47:43 +0000

    20211222-20:47

--- BD/Trabalho1/createinputs.js
+++
@@ -1,7 +0,0 @@
-
-var name='Carlos'
-var paises=['Portugal', 'Angola','Guiné Equatorial', 'Cabo Verde', 'Guiné-Bissau', 'Moçambique' ,'Brasil', 'Macau', 'São Tomé e Príncipe', 'Timor-Leste'];
-
-for(let i in paises){
-    console.log("INSERT INTO gostade VALUES('"+name+"','"+paises[i]+"');")
-}
