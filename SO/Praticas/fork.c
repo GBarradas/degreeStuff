507572170726568dd4c8eda73e65603dd6201994
Gonçalo <gbarradas1@hotmail.com>
2022-02-15 15:48:51 +0000

    20220215-15:48

---
+++ SO/Praticas/fork.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+void forkTest() {
+        puts("TESTE FORK:\n");
+    
+    pid_t pid;
+    
+    pid = fork(); //cria processo
+    
+    if(pid > 0) {
+        printf(" Processo Pai PID: %d \n", pid);
+    }
+    else {
+        printf(" Processo Filho PID: %d \n", pid);
+    }
+    puts("FIM\n");
+}
+
+void main()
+{
+    forkTest();
+}
